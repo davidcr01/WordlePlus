c9a0e1d4be6fa28459d0e9dfc3004f643011b858
David Correa Rodríguez <davidcorreafjb2001@gmail.com>
2023-05-17 19:31:17 +0200

    Securizing the password field

--- Wordle+/django/djapi/serializers.py
+++ Wordle+/django/djapi/serializers.py
@@ -1,10 +1,18 @@
 from django.contrib.auth.models import Group
 from .models import CustomUser, Player
 from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
 
 
 # Serializer related to the CustomUser model. It considers all the fields
 class CustomUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        validated_data['password'] = make_password(password)  # Encrypt the password
+        return super().create(validated_data)
+    
     class Meta:
         model = CustomUser
         fields = '__all__'
