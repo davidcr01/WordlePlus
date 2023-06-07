e13b3faf2fe314d3a07bf25cea2d7f6d94c18546
David Correa <davidcorreafjb2001@gmail.com>
2023-06-07 23:36:55 +0200

    Improval of the StaffCode model

--- Wordle+/django/djapi/models.py
+++ Wordle+/django/djapi/models.py
@@ -28,6 +28,7 @@ class Player(models.Model):
 # will be required if the user wants to be registered as an Event Manager
 class StaffCode(models.Model):
     code = models.CharField(max_length=20)
+    used = models.BooleanField(default=False)
 
     def __str__(self):
         return self.code
